4e7e1edd6228d03e76062e2c2f860860fbdddab1
Aykutak04 <aykutak.04.55@gmail.com>
2025-09-01 23:57:05 +0300

    hata duzeltildi

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { ScrollAreaDemo } from "@/components/HomePageComponents/ScrollAreaDemo";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:3000/api/movies", {
+  const res = await fetch("/api/movies", {
     cache: "no-store", // her istekte güncel veriyi çekmek için
   });
   const movies = await res.json();
