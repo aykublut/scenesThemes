11fb2bcc79c0e2a640a91c5d48e5f35c7db010a5
Aykutak04 <aykutak.04.55@gmail.com>
2025-09-02 00:59:56 +0300

    vercel ve local

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { ScrollAreaDemo } from "@/components/HomePageComponents/ScrollAreaDemo";
 
 export default async function Home() {
-  const res = await fetch("https://scenes-themes.vercel.app/api/movies", {
-    cache: "no-store", // her istekte güncel veriyi çekmek için
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/movies`, {
+    cache: "no-store",
   });
   const movies = await res.json();
+
   return (
     <div className="flex relative flex-col justify-start items-center w-full">
       <ScrollAreaDemo movies={movies} />
