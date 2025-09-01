fbb1983d22056a6d54c8be1026ac592956b94ae9
Aykutak04 <aykutak.04.55@gmail.com>
2025-09-02 00:29:29 +0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import { ScrollAreaDemo } from "@/components/HomePageComponents/ScrollAreaDemo";
 
 export default async function Home() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  const res = await fetch(`${baseUrl}/api/movies`, { cache: "no-store" });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/movies`, {
+    cache: "no-store", // her istekte güncel veriyi çekmek için
+  });
   const movies = await res.json();
-
   return (
     <div className="flex relative flex-col justify-start items-center w-full">
       <ScrollAreaDemo movies={movies} />
